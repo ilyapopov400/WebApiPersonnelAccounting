e50c1846fd11c9ba9512cec170c3c6ac57ade4f6
ilya_popov <il_pop@mail.ru>
2024-06-17 10:23:32 +0300

    Index

--- personnel/app/views.py
+++ personnel/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+
+class Index(TemplateView):
+    '''
+    Стартовая страница приложения
+    '''
+
+    template_name = "app/index.html"
