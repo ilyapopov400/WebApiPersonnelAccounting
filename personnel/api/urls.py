4267cee20ebb405d12a0f41fbed19771dfc4f28d
ilya_popov <il_pop@mail.ru>
2024-06-24 10:40:53 +0300

    Добавил роут в маршрут, class ApiPersonnel(viewsets.ModelViewSet)

--- personnel/api/urls.py
+++ personnel/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
 app_name = 'api'
 
+router = routers.SimpleRouter()  # создание роутера
+router.register(r"", views.ApiPersonnel)  # регистрация роутера
+
 urlpatterns = [
-    path('v1/', views.ApiCreatePersonnel.as_view()),
-    path('v1/<int:pk>/', views.ApiDetailPersonnel.as_view()),
+    path('v1/', include(router.urls)),  # http://127.0.0.1:8000/api/v1/
 ]
--- personnel/api/views.py
+++ personnel/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from app.models import ProfilePersonnelModel
 from . import serializers
@@ -7,22 +7,32 @@ from . import serializers
 
 # Create your views here.
 
-
-class ApiCreatePersonnel(generics.ListCreateAPIView):
+class ApiPersonnel(viewsets.ModelViewSet):
     """
     Список всех карточек
     Создание новой карточки
+    Получение одну карточки
+    Изменение одну карточки
+    Удаление одной карточки
     """
     queryset = ProfilePersonnelModel.objects.all()
     serializer_class = serializers.ProfilePersonnelSerializer
 
-
-class ApiDetailPersonnel(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Получить одну карточку
-    изменить одну карточку
-    удалить одну карточку
-
-    """
-    queryset = ProfilePersonnelModel.objects.all()
-    serializer_class = serializers.ProfilePersonnelSerializer
+# class ApiCreatePersonnel(generics.ListCreateAPIView):
+#     """
+#     Список всех карточек
+#     Создание новой карточки
+#     """
+#     queryset = ProfilePersonnelModel.objects.all()
+#     serializer_class = serializers.ProfilePersonnelSerializer
+#
+#
+# class ApiDetailPersonnel(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     Получить одну карточку
+#     изменить одну карточку
+#     удалить одну карточку
+#
+#     """
+#     queryset = ProfilePersonnelModel.objects.all()
+#     serializer_class = serializers.ProfilePersonnelSerializer
